566caf6a3c003e0eac2f29d56e4e0bba161cab91
poka0117 <96187057+poka0117@users.noreply.github.com>
2021-12-23 10:27:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // 最初のtransition 回避
 $(document).ready(function(){
     $('body').fadeIn();
+    console.log('r');
 })
 
 // header
@@ -55,12 +56,12 @@ $(window).ready(function(){
     let galleryTitleOffset = $('section.gallery h2').offset().top;
     let accesstoffset = $('section.access h2').offset().top/1.2;
     let contactTitleOffset = $('section.contact h2').offset().top/1.2;
-    let galleryimgoffset1 = $('.imgWrapper_1').offset().top;
-    let galleryimgoffset2 = $('.imgWrapper_2').offset().top; 
-    let galleryimgoffset3 = $('.imgWrapper_3').offset().top; 
-    let galleryimgoffset4 = $('.imgWrapper_4').offset().top; 
-    let galleryimgoffset5 = $('.imgWrapper_5').offset().top;
-    let galleryimgoffset6 = $('.imgWrapper_6').offset().top; 
+    let galleryimgoffset1 = $('.imgWrapper_1').offset().top-80;
+    let galleryimgoffset2 = $('.imgWrapper_2').offset().top-80; 
+    let galleryimgoffset3 = $('.imgWrapper_3').offset().top-80; 
+    let galleryimgoffset4 = $('.imgWrapper_4').offset().top-80; 
+    let galleryimgoffset5 = $('.imgWrapper_5').offset().top-80;
+    let galleryimgoffset6 = $('.imgWrapper_6').offset().top-80; 
     $(window).scroll(function(){
         let scroll = $(this).scrollTop() + $(window).height()/1.3;
         if(scroll > titleOffset){

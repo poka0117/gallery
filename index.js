124e2af66b47db41bb6f0ed9d1b12db32cca8551
poka0117 <96187057+poka0117@users.noreply.github.com>
2021-12-23 14:38:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -66,11 +66,11 @@ $(window).ready(function(){
     var galleryimgoffset6 = $('.imgWrapper_6').offset().top; 
     }else{
     var galleryimgoffset1 = $('.imgWrapper_1').offset().top;
-    var galleryimgoffset2 = $('.imgWrapper_2').offset().top/1; 
-    var galleryimgoffset3 = $('.imgWrapper_3').offset().top/1; 
-    var galleryimgoffset4 = $('.imgWrapper_4').offset().top/1; 
-    var galleryimgoffset5 = $('.imgWrapper_5').offset().top/1;
-    var galleryimgoffset6 = $('.imgWrapper_6').offset().top/1; 
+    var galleryimgoffset2 = $('.imgWrapper_2').offset().top; 
+    var galleryimgoffset3 = $('.imgWrapper_3').offset().top/1.1; 
+    var galleryimgoffset4 = $('.imgWrapper_4').offset().top/1.12; 
+    var galleryimgoffset5 = $('.imgWrapper_5').offset().top/1.14;
+    var galleryimgoffset6 = $('.imgWrapper_6').offset().top/1.15; 
     }
 
     $(window).scroll(function(){
